041305b04ecf23db55233cd878a2141512bd590a
Federico Gomez <gomez00federico@gmail.com>
2024-07-24 12:04:48 -0300

    import error fixing

--- youtube_pydantic_models/__init__.py
+++ youtube_pydantic_models/__init__.py
@@ -1,5 +1,45 @@
-import youtube_pydantic_models._parts
-import youtube_pydantic_models._subparts
+from youtube_pydantic_models._parts.audit_details import BaseAuditDetails
+from youtube_pydantic_models._parts.branding_settings import BaseBrandingSettings
+from youtube_pydantic_models._parts.content_details import (
+    ChannelContentDetails,
+    PlaylistContentDetails,
+    VideoContentDetails
+)
+from youtube_pydantic_models._parts.content_owner_details import BaseContentOwnerDetails
+from youtube_pydantic_models._parts.file_details import BaseFileDetails
+from youtube_pydantic_models._parts.id import SearchId
+from youtube_pydantic_models._parts.live_streaming_details import BaseLiveStreamingDetails
+from youtube_pydantic_models._parts.page_info import BasePageInfo
+from youtube_pydantic_models._parts.player import (
+    BasePlayer,
+    VideoPlayer
+)
+from youtube_pydantic_models._parts.processing_details import BaseProcessingDetails
+from youtube_pydantic_models._parts.recording_details import BaseRecordingDetails
+from youtube_pydantic_models._parts.snippet import (
+    BaseSnippet,
+    SearchSnippet,
+    ChannelSnippet,
+    PlaylistSnippet,
+    VideoSnippet
+)
+from youtube_pydantic_models._parts.statistics import (
+    BaseStatistics,
+    ChannelStatistics,
+    VideoStatistics
+)
+from youtube_pydantic_models._parts.status import (
+    BaseStatus,
+    SharedStatus,
+    ChannelStatus,
+    VideoStatus
+)
+from youtube_pydantic_models._parts.suggestions import BaseSuggestions
+from youtube_pydantic_models._parts.topic_details import (
+    BaseTopicDetails,
+    VideoTopicDetails
+)
+
 from youtube_pydantic_models.base_model_config import get_base_model_config
 from youtube_pydantic_models.base_resource import YoutubeBaseResource
 from youtube_pydantic_models.channel_resource import YoutubeChannelResource
