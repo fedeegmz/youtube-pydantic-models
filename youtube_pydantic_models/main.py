924b0bb357c4ebd6744dbd32e61abf0f1718769d
Federico Gomez <gomez00federico@gmail.com>
2024-07-25 15:59:28 -0300

    base YoutubeClient

--- youtube_pydantic_models/main.py
+++ youtube_pydantic_models/main.py
@@ -1,31 +1,107 @@
-import googleapiclient.discovery
+import requests
 
 
 class YoutubeClient:
     def __init__(self, api_key: str) -> None:
-        API_SERVICE_NAME = "youtube"
-        API_VERSION = "v3"
+        if not api_key:
+            raise Exception("API_KEY param is required")
         
-        self.client = self.login(
-            API_SERVICE_NAME,
-            API_VERSION,
-            api_key
+        self._api_key: str = api_key
+        self._api_version: str = "v3"
+        self._api_service_name: str = "youtube"
+        self._api_base_path: str = f"https://www.googleapis.com/"
+        self._api_path: str = f"{self._api_base_path}{self._api_service_name}/{self._api_version}/"
+    
+    def get_api_service_name(self) -> str:
+        return self._api_service_name
+    
+    def get_api_version(self) -> str:
+        return self._api_version
+    
+    def get_channel(
+        self,
+        id: str,
+        part: str
+    ) -> dict:
+        available_part = "snippet, contentDetails, statistics, topicDetails, status, brandingSettings, contentOwnerDetails"
+        params = {
+            'id': id,
+            'part': part
+        }
+
+        response = self._get_request(
+            endpoint = "channels",
+            params = params
         )
+        return response
+
+    def get_playlist(
+        self,
+        id: str,
+        part: str
+    ) -> dict:
+        available_part = "snippet, contentDetails, player, status, id, localizations"
+        params = {
+            'id': id,
+            'part': part
+        }
+
+        response = self._get_request(
+            endpoint = "playlists",
+            params = params
+        )
+        return response
+
+    def get_video(
+        self,
+        id: str,
+        part: str
+    ) -> dict:
+        available_part = "snippet, contentDetails, statistics, topicDetails, status, player, recordingDetails, localizations, liveStreamingDetails"
+        params = {
+            'id': id,
+            'part': part
+        }
+
+        response = self._get_request(
+            endpoint = "videos",
+            params = params
+        )
+        return response
+
+    def search(
+        self,
+        channel_id: str,
+        part: str,
+        type: str
+    ) -> dict:
+        available_part = "id, snippet"
+        params = {
+            'id': channel_id,
+            'part': part,
+            'type': type
+        }
+
+        response = self._get_request(
+            endpoint = "search",
+            params = params
+        )
+        return response
     
-    def login(
-        api_service_name: str,
-        api_version: str,
-        api_key: str
-    ):
-        try:
-            client = googleapiclient.discovery.build(
-                api_service_name,
-                api_version,
-                developerKey = api_key
-            )
-        except:
-            raise Exception("Youtube client not connected")
-        return client
+    def _get_request(
+        self,
+        endpoint: str,
+        params: dict
+    ) -> dict | list:
+        response = requests.get(
+            self._api_path + endpoint,
+            params=self._set_key_param(params)
+        )
+
+        if response.status_code != 200:
+            return None
+        return response.json()
     
-    def get_client(self):
-        return self.client
+    def _set_key_param(self, params: dict) -> dict:
+        params['key'] = self._api_key
+        return params
